78e8c8972270514600d1b61e74b1a440f03c55a2
Tiago Castro <tiagocastro602@gmail.com>
2022-10-10 18:11:48 -0300

    stitches config/global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,19 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { getCssText } from "../styles";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Preconnect fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+
+        {/* Fonts */}
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+
+        {/* SSR Stitches */}
+        <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}} />
       </Head>
 
       <body>
---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import {createStitches} from '@stitches/react'
+
+export const {config, theme, createTheme, styled, css, globalCss, keyframes, getCssText } = createStitches({
+  theme: {
+    colors: {
+      white: '#ffffff',
+      green300: '#00b37e',
+      green500: '#00875f',
+      gray100: '#e1e1e6',
+      gray300: '#c4c4cc',
+      gray800: '#202024',
+      gray900: '#121214',
+    }
+  }
+})
